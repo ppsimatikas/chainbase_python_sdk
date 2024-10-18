586733a595e7951bb30a67e8d843d023756eeb97
panosdb <ppsimatikas@gmail.com>
2024-10-18 10:59:24 +0300

    Add better setup

--- setup.py
+++ setup.py
@@ -3,5 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name="chainbase_python_sdk",
     version="0.0.1",
+    description="A Python SDK for interacting with Chainbase APIs.",
+    author="Panos",
+    url="https://github.com/ppsimatikas/chainbase_python_sdk",
     packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "requests==2.31.0",
+        "pandas==2.1.3"
+    ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
