202dedcbb56f14cbff0cacc41964908f3fbc23e4
panosdb <ppsimatikas@gmail.com>
2024-10-18 11:21:06 +0300

    Fix pip install

--- setup.py
+++ setup.py
@@ -11,11 +11,5 @@ setup(
         "requests==2.32.3",
         "pandas==2.1.3"
     ],
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    license="MIT"
 )
