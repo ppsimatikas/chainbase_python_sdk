25775435d43bd6c4a9689e48863941b5b3999111
panosdb <ppsimatikas@gmail.com>
2024-10-18 11:36:16 +0300

    Fix pip install

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name="chainbase_python_sdk",
+    name="chainbase_sdk",
     version="0.0.1",
     description="A Python SDK for interacting with Chainbase APIs.",
     author="Panos",
     url="https://github.com/ppsimatikas/chainbase_python_sdk",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(include=["src"], exclude=["tests"]),
     install_requires=[
         "requests==2.32.3",
         "pandas==2.1.3"
